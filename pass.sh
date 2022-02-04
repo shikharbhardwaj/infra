94ceaff3f6d8591eb18ea18e56c2dd0954bfdb97
shikharbhardwaj <shikhar.bhardwaj@codenation.co.in>
2022-02-04 22:44:46 +0530

    Make pass.sh work with bitwarden

--- pass.sh
+++ pass.sh
@@ -2,7 +2,4 @@
 
 # Keychain query fields.
 # LABEL is the value you put for "Keychain Item Name" in Keychain.app.
-LABEL="ansible-vault-password"
-ACCOUNT_NAME="notthebee"
-
-/usr/bin/security find-generic-password -w -a "$ACCOUNT_NAME" -l "$LABEL"
+bw get password Ansible
